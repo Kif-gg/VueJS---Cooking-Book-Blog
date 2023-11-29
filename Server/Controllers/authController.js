064ba393abed69b84edb8a3c3e839be7f5260c77
Akif Syuleyman <f17.8oy@gmail.com>
2023-11-29 14:03:23 +0200

    Added authController + post to register function

---
+++ Server/Controllers/authController.js
@@ -0,0 +1,17 @@
+const { register, login, logout, changeEmail, changePassword, deleteUser, getUserById } = require("../Services/userService");
+const { parseError } = require("../Util/errorParser");
+const { allowGuestsOnly, allowUsersOnly, allowAdminsOnly } = require("../Middlewares/guard");
+
+const authController = require("express").Router();
+
+authController.post("/register", allowGuestsOnly(), async (req, res) => {
+    try {
+        const token = await register(req.body.username, req.body.email, req.body.password, req.body.repass);
+        res.cookie("AUTHORIZATION", token.accessToken, { httpOnly: true });
+        res.json(token);
+    } catch (error) {
+        const message = parseError(error);
+        res.cookie("AUTHORIZATION", "alabala", { maxAge: 0 });
+        res.status(400).json({ message });
+    }
+});
