f3c1374b45a15394bd9366905fd66cedd50fdc39
Akif Syuleyman <f17.8oy@gmail.com>
2023-11-29 13:48:50 +0200

    Added paths/requests guard middleware

---
+++ Server/Middlewares/guard.js
@@ -0,0 +1,35 @@
+function allowGuestsOnly() {
+    return (req, res, next) => {
+        if (!req.user) {
+            next();
+        } else {
+            res.status(400).json({ message: "You are already logged in!" });
+        }
+    };
+};
+
+function allowUsersOnly() {
+    return (req, res, next) => {
+        if (req.user) {
+            next();
+        } else {
+            res.status(401).json({ message: "Log in is required!" });
+        }
+    };
+};
+
+function allowAdminsOnly() {
+    return (req, res, next) => {
+        if (req.user && req.user.pin && !req.user.username) {
+            next();
+        } else {
+            res.status(400).json({ message: "You have no permission!" });
+        }
+    }
+};
+
+module.exports = {
+    allowGuestsOnly,
+    allowUsersOnly,
+    allowAdminsOnly
+};
