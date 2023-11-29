4219b63fa7b7de47a3130319adc5ca7d960335be
Akif Syuleyman <f17.8oy@gmail.com>
2023-11-29 14:06:17 +0200

    Added post function to login

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -15,3 +15,15 @@ authController.post("/register", allowGuestsOnly(), async (req, res) => {
         res.status(400).json({ message });
     }
 });
+
+authController.post("/login", allowGuestsOnly(), async (req, res) => {
+    try {
+        const token = await login(req.body.username, req.body.password);
+        res.cookie("AUTHORIZATION", token.accessToken, { httpOnly: true });
+        res.json(token);
+    } catch (error) {
+        const message = parseError(error);
+        res.cookie("AUTHORIZATION", "alabala", { maxAge: 0 });
+        res.status(400).json({ message });
+    }
+});
