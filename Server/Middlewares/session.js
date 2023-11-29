8e34a44a7699b193ac6c579dad1ccf4c5a9ce4fe
Akif Syuleyman <f17.8oy@gmail.com>
2023-11-29 12:39:17 +0200

    Added middleware for session

---
+++ Server/Middlewares/session.js
@@ -0,0 +1,18 @@
+const { parseToken } = require('../Services/userService');
+
+module.exports = () => async (req, res, next) => {
+    const token = req.cookies['Authorization'];
+
+    if (token) {
+        try {
+            const payload = await parseToken(token);
+            req.user = payload;
+            req.token = token;
+        } catch (error) {
+            res.cookie('Authorization', 'alabala', { maxAge: 0 });
+            return res.status(401).json({ message: error.message });
+        }
+    }
+
+    next();
+};
