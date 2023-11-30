04f92d095412620c6f00f2cc8948ec04002742fd
Akif Syuleyman <f17.8oy@gmail.com>
2023-11-30 17:00:12 +0200

    Added createRecipe

--- Server/Services/adminService.js
+++ Server/Services/adminService.js
@@ -5,6 +5,7 @@ const Recipe = require("../Models/Recipe");
 const bcrypt = require("bcrypt");
 
 const { createToken } = require("../Util/tokenManager");
+const { validateImgUrl } = require("../Util/inputValidator");
 
 async function login(formData) {
     try {
@@ -23,6 +24,62 @@ async function login(formData) {
         }
 
         return createToken(admin);
+    } catch (error) {
+        throw error;
+    }
+};
+
+async function logout(token) {
+    await BlacklistedToken.create({ token });
+};
+
+async function createRecipe(formData) {
+    try {
+        const { imgUrl, name, category, description, productsNeeded, instructions } = formData;
+        let errorStack = "";
+        if (!imgUrl) {
+            errorStack += "Image URL is required!\r\n";
+        } else if (!validateImgUrl(imgUrl)) {
+            errorStack += "URL is not valid!\r\n";
+        }
+        if (!name) {
+            errorStack += "Name is required!\r\n"
+        } else if (name.length < 3) {
+            errorStack += "Name length must be at least 3 characters!\r\n";
+        } else if (name.length > 100) {
+            errorStack += "Name length must not exceed 100 characters!\r\n";
+        }
+        if (!category) {
+            errorStack += "Category is required!\r\n";
+        }
+        if (!description) {
+            errorStack += "Description is required!\r\n";
+        } else if (description.length < 10) {
+            errorStack += "Description length must be at least 10 characters!\r\n";
+        } else if (description.length > 200) {
+            errorStack += "Description length must not exceed 200 characters!\r\n";
+        }
+        if (!productsNeeded) {
+            errorStack += "Products are required!\r\n";
+        }
+        if (!instructions) {
+            errorStack += "Instructions are required!\r\n";
+        } else if (instructions.length < 50) {
+            errorStack += "Instructions length must be at least 50 characters!\r\n";
+        }
+
+        if (errorStack.length > 0) {
+            throw new Error(errorStack);
+        }
+
+        return Recipe.create({
+            imgUrl,
+            name,
+            category,
+            description,
+            productsNeeded,
+            instructions
+        });
     } catch (error) {
         throw error;
     }
