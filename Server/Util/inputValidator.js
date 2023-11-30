b7d842d1de5807bade70fbee02febab2f4e31ebb
Akif Syuleyman <f17.8oy@gmail.com>
2023-11-30 16:36:02 +0200

    Added validator for URL

--- Server/Util/inputValidator.js
+++ Server/Util/inputValidator.js
@@ -1,14 +1,20 @@
 function validateUsername(username) {
     const pattern = /^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-z0-9._]+(?<![_.])$/i;
     return pattern.test(username);
-}
+};
 
 function validateEmail(email) {
     const pattern = /^[a-z0-9._-]{3,20}@[a-z0-9.-]{3,20}\.[a-z]{2,6}$/i;
     return pattern.test(email);
-}
+};
+
+function validateImgUrl(imgUrl) {
+    const pattern = /^https?:\/\/(?:www\.)?[\w-]{3,30}\.[a-z]{2,10}(?:[\w\W]{1,300})?$/i;
+    return pattern.test(imgUrl);
+};
 
 module.exports = {
     validateUsername,
-    validateEmail
+    validateEmail,
+    validateImgUrl
 };
