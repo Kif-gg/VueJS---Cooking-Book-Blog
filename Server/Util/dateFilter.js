6c2df24d47264094b58548824d8f0b00eddce73f
Akif Syuleyman <f17.8oy@gmail.com>
2023-12-02 13:52:39 +0200

    Added util functions to filter database records

---
+++ Server/Util/dateFilter.js
@@ -0,0 +1,37 @@
+function getStartOfWeek(date) {
+    const currentDay = date.getDay();
+    const diff = date.getDate() - currentDay + (currentDay == 1 ? 0 : (currentDay == 0 ? -6 : 1));
+    const startOfWeek = new Date(date);
+    startOfWeek.setDate(diff);
+    startOfWeek.setHours(0, 0, 0, 0);
+    return startOfWeek;
+};
+
+function getEndOfWeek(date) {
+    const endOfWeek = new Date(date);
+    endOfWeek.setDate(date.getDate() - date.getDay() + 7);
+    endOfWeek.setHours(23, 59, 59, 999);
+    return endOfWeek;
+};
+
+function getTodaysRecords(array) {
+    const today = new Date(Date.now());
+    today.setHours(0, 0, 0, 0);
+    const endOfDay = new Date(today);
+    endOfDay.setHours(23, 59, 59, 999);
+
+    return array.filter((date) => date >= today && date <= endOfDay);
+};
+
+function getThisWeeksRecords(array) {
+    const today = new Date(Date.now());
+    const startOfWeek = getStartOfWeek(today);
+    const endOfWeek = getEndOfWeek(today);
+
+    return array.filter((date) => date >= startOfWeek && date <= endOfWeek);
+};
+
+module.exports = {
+    getTodaysRecords,
+    getThisWeeksRecords
+};
