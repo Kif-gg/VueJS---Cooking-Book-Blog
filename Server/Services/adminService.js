dac79be1d06c75e2186d72432527b48827f313d1
Akif Syuleyman <f17.8oy@gmail.com>
2023-12-02 14:53:03 +0200

    Removed redundant try-catch blocks

--- Server/Services/adminService.js
+++ Server/Services/adminService.js
@@ -156,65 +156,61 @@ async function createRecipe(formData) {
 };
 
 async function editRecipe(recipe, formData) {
-    try {
-        const { imgUrl, name, category, description, productsNeeded, instructions } = formData;
+    const { imgUrl, name, category, description, productsNeeded, instructions } = formData;
 
-        let errorStack = "";
-        if (!imgUrl) {
-            errorStack += "Image URL is required!\r\n";
-        } else if (!validateImgUrl(imgUrl)) {
-            errorStack += "URL is not valid!\r\n";
-        }
-        if (!name) {
-            errorStack += "Name is required!\r\n"
-        } else if (name.length < 3) {
-            errorStack += "Name length must be at least 3 characters!\r\n";
-        } else if (name.length > 100) {
-            errorStack += "Name length must not exceed 100 characters!\r\n";
-        }
-        if (!category) {
-            errorStack += "Category is required!\r\n";
-        }
-        if (!description) {
-            errorStack += "Description is required!\r\n";
-        } else if (description.length < 10) {
-            errorStack += "Description length must be at least 10 characters!\r\n";
-        } else if (description.length > 200) {
-            errorStack += "Description length must not exceed 200 characters!\r\n";
-        }
-        if (!productsNeeded) {
-            errorStack += "Products are required!\r\n";
-        }
-        if (!instructions) {
-            errorStack += "Instructions are required!\r\n";
-        } else if (instructions.length < 50) {
-            errorStack += "Instructions length must be at least 50 characters!\r\n";
-        }
-
-        if (recipe.imgUrl == imgUrl
-            && recipe.name == name
-            && recipe.category == category
-            && recipe.description == description
-            && recipe.productsNeeded == productsNeeded
-            && recipe.instructions == instructions) {
-            errorStack += "You must change at least 1 field!\r\n";
-        }
-
-        if (errorStack.length > 0) {
-            throw new Error(errorStack);
-        }
-
-        recipe.imgUrl = imgUrl;
-        recipe.name = name;
-        recipe.category = category;
-        recipe.description = description;
-        recipe.productsNeeded = productsNeeded;
-        recipe.instructions = instructions;
-
-        return recipe.save();
-    } catch (error) {
-        throw error;
+    let errorStack = "";
+    if (!imgUrl) {
+        errorStack += "Image URL is required!\r\n";
+    } else if (!validateImgUrl(imgUrl)) {
+        errorStack += "URL is not valid!\r\n";
     }
+    if (!name) {
+        errorStack += "Name is required!\r\n"
+    } else if (name.length < 3) {
+        errorStack += "Name length must be at least 3 characters!\r\n";
+    } else if (name.length > 100) {
+        errorStack += "Name length must not exceed 100 characters!\r\n";
+    }
+    if (!category) {
+        errorStack += "Category is required!\r\n";
+    }
+    if (!description) {
+        errorStack += "Description is required!\r\n";
+    } else if (description.length < 10) {
+        errorStack += "Description length must be at least 10 characters!\r\n";
+    } else if (description.length > 200) {
+        errorStack += "Description length must not exceed 200 characters!\r\n";
+    }
+    if (!productsNeeded) {
+        errorStack += "Products are required!\r\n";
+    }
+    if (!instructions) {
+        errorStack += "Instructions are required!\r\n";
+    } else if (instructions.length < 50) {
+        errorStack += "Instructions length must be at least 50 characters!\r\n";
+    }
+
+    if (recipe.imgUrl == imgUrl
+        && recipe.name == name
+        && recipe.category == category
+        && recipe.description == description
+        && recipe.productsNeeded == productsNeeded
+        && recipe.instructions == instructions) {
+        errorStack += "You must change at least 1 field!\r\n";
+    }
+
+    if (errorStack.length > 0) {
+        throw new Error(errorStack);
+    }
+
+    recipe.imgUrl = imgUrl;
+    recipe.name = name;
+    recipe.category = category;
+    recipe.description = description;
+    recipe.productsNeeded = productsNeeded;
+    recipe.instructions = instructions;
+
+    return recipe.save();
 };
 
 async function deleteRecipe(recipe) {
--- Server/Services/userService.js
+++ Server/Services/userService.js
@@ -8,88 +8,80 @@ const { createToken } = require("../Util/tokenManager");
 const { getSingleInstance } = require("../Models/Dashboard");
 
 async function register(formData) {
-    try {
-        let errorStack = "";
-        const { username, email, password, repass } = formData;
-        const existingUsername = await User.findOne({ username }).collation({ locale: "en", strength: 2 });
-        const existingEmail = await User.findOne({ email }).collation({ locale: "en", strength: 2 });
+    let errorStack = "";
+    const { username, email, password, repass } = formData;
+    const existingUsername = await User.findOne({ username }).collation({ locale: "en", strength: 2 });
+    const existingEmail = await User.findOne({ email }).collation({ locale: "en", strength: 2 });
 
-        if (existingUsername) {
-            errorStack += "Username already exists!\r\n";
-        }
-        if (existingEmail) {
-            errorStack += "Email already registered!\r\n";
-        }
-        if (!username || username.length == 0) {
-            errorStack += "Username is required!\r\n";
-        } else if (username.length < 3) {
-            errorStack += "Username length must be at least 3 characters!\r\n"
-        } else if (username.length > 20) {
-            errorStack += "Username length must not exceed 20 characters!\r\n"
-        } else if (!validateUsername(username)) {
-            errorStack += "Username is not valid!\r\nAllowed characters: A to Z, a to z, 0 to 9, _ (except start/end) and . (except start/end)\r\n";
-        }
-        if (!email) {
-            errorStack += "Email is required!\r\n";
-        } else if (!validateEmail(email)) {
-            errorStack += "Email is not valid!\r\n"
-        }
-        if (!password) {
-            errorStack += "Password is required!\r\n";
-        } else if (password.length < 6) {
-            errorStack += "Password length must be at least 6 characters!\r\n";
-        } else if (password.length > 256) {
-            errorStack += "Password length must not exceed 256 characters!\r\n"
-        } else {
-            if (password.includes(" ")) {
-                errorStack += "Password must not contain whitespaces!\r\n";
-            }
-            if (!repass || repass != password) {
-                errorStack += "Repeated password does not match the original!\r\n"
-            }
-        }
-        if (errorStack.length > 0) {
-            throw new Error(errorStack);
-        }
-
-        const user = await User.create({
-            username,
-            email,
-            hashedPassword: await bcrypt.hash(password, 12)
-        });
-
-        const DASHBOARD = await getSingleInstance();
-        console.log(DASHBOARD + "From uSer");
-        DASHBOARD.usersDashboard.totalRegistered.push(new Date(Date.now()));
-        DASHBOARD.save();
-        return createToken(user);
-    } catch (error) {
-        throw error;
+    if (existingUsername) {
+        errorStack += "Username already exists!\r\n";
     }
+    if (existingEmail) {
+        errorStack += "Email already registered!\r\n";
+    }
+    if (!username || username.length == 0) {
+        errorStack += "Username is required!\r\n";
+    } else if (username.length < 3) {
+        errorStack += "Username length must be at least 3 characters!\r\n"
+    } else if (username.length > 20) {
+        errorStack += "Username length must not exceed 20 characters!\r\n"
+    } else if (!validateUsername(username)) {
+        errorStack += "Username is not valid!\r\nAllowed characters: A to Z, a to z, 0 to 9, _ (except start/end) and . (except start/end)\r\n";
+    }
+    if (!email) {
+        errorStack += "Email is required!\r\n";
+    } else if (!validateEmail(email)) {
+        errorStack += "Email is not valid!\r\n"
+    }
+    if (!password) {
+        errorStack += "Password is required!\r\n";
+    } else if (password.length < 6) {
+        errorStack += "Password length must be at least 6 characters!\r\n";
+    } else if (password.length > 256) {
+        errorStack += "Password length must not exceed 256 characters!\r\n"
+    } else {
+        if (password.includes(" ")) {
+            errorStack += "Password must not contain whitespaces!\r\n";
+        }
+        if (!repass || repass != password) {
+            errorStack += "Repeated password does not match the original!\r\n"
+        }
+    }
+    if (errorStack.length > 0) {
+        throw new Error(errorStack);
+    }
+
+    const user = await User.create({
+        username,
+        email,
+        hashedPassword: await bcrypt.hash(password, 12)
+    });
+
+    const DASHBOARD = await getSingleInstance();
+    console.log(DASHBOARD + "From uSer");
+    DASHBOARD.usersDashboard.totalRegistered.push(new Date(Date.now()));
+    DASHBOARD.save();
+    return createToken(user);
 };
 
 async function login(formData) {
-    try {
-        const { username, password } = formData;
-        const user = await User.findOne({ username }).collation({ locale: "en", strength: 2 });
+    const { username, password } = formData;
+    const user = await User.findOne({ username }).collation({ locale: "en", strength: 2 });
 
-        if (!user) {
-            throw new Error("Incorrect username or password!");
-        }
-
-        if (user.blocked) {
-            throw new Error("This account is blocked! Contact support via email to unblock Your account!");
-        }
-
-        const match = bcrypt.compare(password, user.hashedPassword);
-        if (!match) {
-            throw new Error("Incorrect username or password!");
-        }
-
-        return createToken(user);
-    } catch (error) {
-        throw error;
+    if (!user) {
+        throw new Error("Incorrect username or password!");
     }
+
+    if (user.blocked) {
+        throw new Error("This account is blocked! Contact support via email to unblock Your account!");
+    }
+
+    const match = bcrypt.compare(password, user.hashedPassword);
+    if (!match) {
+        throw new Error("Incorrect username or password!");
+    }
+
+    return createToken(user);
 };
 
 async function logout(token) {
@@ -97,113 +89,101 @@ async function logout(token) {
 };
 
 async function changeEmail(user, formData) {
-    try {
-        const { oldEmail, newEmail, reEmail } = formData;
-        let errorStack = "";
-        if (!oldEmail) {
-            errorStack += "Old email is required!\r\n";
-        }
-        if (user.email.toLowerCase() != oldEmail.toLowerCase()) {
-            errorStack += "Old email is wrong!\r\n";
-        }
-        if (!newEmail) {
-            errorStack += "New email is required!\r\n";
-        }
-        if (user.email.toLowerCase() == newEmail.toLowerCase()) {
-            errorStack += "New email can't be Your old email!\r\n";
-        }
-        if (!validateEmail(newEmail)) {
-            errorStack += "New email is not valid!\r\n";
-        }
-        if (!reEmail || newEmail != reEmail) {
-            errorStack += "Repeated email does not match the original!\r\n";
-        }
-        if (errorStack.length > 0) {
-            throw new Error(errorStack);
-        }
-
-        user.email = newEmail;
-        return user.save();
-    } catch (error) {
-        throw error;
+    const { oldEmail, newEmail, reEmail } = formData;
+    let errorStack = "";
+    if (!oldEmail) {
+        errorStack += "Old email is required!\r\n";
     }
+    if (user.email.toLowerCase() != oldEmail.toLowerCase()) {
+        errorStack += "Old email is wrong!\r\n";
+    }
+    if (!newEmail) {
+        errorStack += "New email is required!\r\n";
+    }
+    if (user.email.toLowerCase() == newEmail.toLowerCase()) {
+        errorStack += "New email can't be Your old email!\r\n";
+    }
+    if (!validateEmail(newEmail)) {
+        errorStack += "New email is not valid!\r\n";
+    }
+    if (!reEmail || newEmail != reEmail) {
+        errorStack += "Repeated email does not match the original!\r\n";
+    }
+    if (errorStack.length > 0) {
+        throw new Error(errorStack);
+    }
+
+    user.email = newEmail;
+    return user.save();
 };
 
 
 async function changePassword(user, formData) {
-    try {
-        const { oldPassword, newPassword, repass } = formData;
-        let errorStack = "";
-        if (!oldPassword) {
-            errorStack += "Old password is required!\r\n";
-        }
-        const match = await bcrypt.compare(oldPassword, user.hashedPassword);
-        if (!match) {
-            errorStack += "Old password is wrong!\r\n";
-        }
-        if (!newPassword) {
-            errorStack += "New password is required!\r\n";
-        } else if (newPassword.length < 6) {
-            errorStack += "New password length must be at least 6 characters!\r\n";
-        } else if (newPassword.length > 256) {
-            errorStack += "New password length must not exceed 256 characters!\r\n";
-        } else {
-            if (newPassword.includes(" ")) {
-                errorStack += "Password must not contain whitespaces!\r\n";
-            }
-            if (oldPassword == newPassword) {
-                errorStack += "New password can't be Your old password!\r\n";
-            }
-            if (!repass || newPassword != repass) {
-                errorStack += "Repeated password does not match the original!\r\n";
-            }
-        }
-
-        if (errorStack.length > 0) {
-            throw new Error(errorStack);
-        }
-
-        user.hashedPassword = await bcrypt.hash(newPassword, 12);
-        return user.save();
-    } catch (error) {
-        throw error;
+    const { oldPassword, newPassword, repass } = formData;
+    let errorStack = "";
+    if (!oldPassword) {
+        errorStack += "Old password is required!\r\n";
     }
+    const match = await bcrypt.compare(oldPassword, user.hashedPassword);
+    if (!match) {
+        errorStack += "Old password is wrong!\r\n";
+    }
+    if (!newPassword) {
+        errorStack += "New password is required!\r\n";
+    } else if (newPassword.length < 6) {
+        errorStack += "New password length must be at least 6 characters!\r\n";
+    } else if (newPassword.length > 256) {
+        errorStack += "New password length must not exceed 256 characters!\r\n";
+    } else {
+        if (newPassword.includes(" ")) {
+            errorStack += "Password must not contain whitespaces!\r\n";
+        }
+        if (oldPassword == newPassword) {
+            errorStack += "New password can't be Your old password!\r\n";
+        }
+        if (!repass || newPassword != repass) {
+            errorStack += "Repeated password does not match the original!\r\n";
+        }
+    }
+
+    if (errorStack.length > 0) {
+        throw new Error(errorStack);
+    }
+
+    user.hashedPassword = await bcrypt.hash(newPassword, 12);
+    return user.save();
 };
 
 async function deleteUser(user, formData) {
-    try {
-        const { username, email, password, repass } = formData;
-        let errorStack = "";
-        if (!username) {
-            errorStack += "Username is required!\r\n";
-        } else if (user.username.toLowerCase() != username.toLowerCase()) {
-            errorStack += "Incorrect username\r\n";
-        }
-        if (!email) {
-            errorStack += "Email is required!\r\n";
-        } else if (user.email.toLowerCase() != email.toLowerCase()) {
-            errorStack += "Incorrect email!\r\n";
-        }
-        const match = bcrypt.compare(password, user.hashedPassword);
-        if (!password) {
-            errorStack += "Password is required!\r\n";
-        } else if (!match) {
-            errorStack += "Incorrect password!\r\n";
-        } else if (!repass || repass != password) {
-            errorStack += "Repeated password does not match the original!\r\n";
-        }
-
-        if (errorStack.length > 0) {
-            throw new Error(errorStack);
-        }
-
-        const DASHBOARD = await getSingleInstance();
-        DASHBOARD.usersDashboard.totalDeleted.push(new Date(Date.now()));
-        DASHBOARD.save();
-        return User.findByIdAndDelete(user._id);
-    } catch (error) {
-        throw error;
+    const { username, email, password, repass } = formData;
+    let errorStack = "";
+    if (!username) {
+        errorStack += "Username is required!\r\n";
+    } else if (user.username.toLowerCase() != username.toLowerCase()) {
+        errorStack += "Incorrect username\r\n";
     }
+    if (!email) {
+        errorStack += "Email is required!\r\n";
+    } else if (user.email.toLowerCase() != email.toLowerCase()) {
+        errorStack += "Incorrect email!\r\n";
+    }
+    const match = bcrypt.compare(password, user.hashedPassword);
+    if (!password) {
+        errorStack += "Password is required!\r\n";
+    } else if (!match) {
+        errorStack += "Incorrect password!\r\n";
+    } else if (!repass || repass != password) {
+        errorStack += "Repeated password does not match the original!\r\n";
+    }
+
+    if (errorStack.length > 0) {
+        throw new Error(errorStack);
+    }
+
+    const DASHBOARD = await getSingleInstance();
+    DASHBOARD.usersDashboard.totalDeleted.push(new Date(Date.now()));
+    DASHBOARD.save();
+    return User.findByIdAndDelete(user._id);
 };
 
 
