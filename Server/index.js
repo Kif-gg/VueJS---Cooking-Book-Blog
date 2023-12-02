1dd0209aa5ae4c24e26e1ee63de900d140fdd124
Akif Syuleyman <f17.8oy@gmail.com>
2023-12-02 22:52:26 +0200

    Added imports +dashboard +controllers, minor fixes

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-
+const session = require("./Middlewares/session");
 const trimBody = require("./Middlewares/trimBody");
+const { getSingleInstance } = require("./Models/Dashboard");
+const userController = require("./Controllers/userController");
+const recipeController = require("./Controllers/recipeController");
+const adminController = require("./Controllers/adminController");
 
 const connectionStr = "mongodb://127.0.0.1:27017/VueJS-Project-Defence";
 
@@ -14,18 +18,26 @@ async function start() {
         console.log("DATABASE OPERATIONAL");
 
         const app = express();
+        const DASHBOARD = await getSingleInstance();
+        app.locals.DASHBOARD = DASHBOARD;
 
         app.use(express.json());
         app.use(cookieParser());
         app.use(trimBody());
+        app.use(session());
 
-        app.all("/REST-TEST/*", (req, res) => {
+        app.all("/REST-TEST*", (req, res) => {
             res.json({ message: `REST SERVICE OPERATIONAL => URL PATH: ${req.url}` });
         });
+
+        app.use("/users", userController);
+        app.use("/recipes", recipeController);
+        app.use("/secret-path/admin", adminController);
 
         app.listen(3030, () => console.log("REST SERVICE STARTED SUCCESSFULLY"))
     } catch (error) {
         console.log(error);
         console.log(error.message);
+        process.exit(1);
     }
 };
