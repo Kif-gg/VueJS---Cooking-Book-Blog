dd1f6c84323a94f5f89537bc31de781b375f501b
Akif Syuleyman <f17.8oy@gmail.com>
2023-12-02 08:07:40 +0200

    Created dashboard model returning single instance

---
+++ Server/Models/Dashboard.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+
+const dashboardSchema = new Schema({
+    usersDashboard: {
+        totalRegistered: {
+            type: [Date],
+            default: []
+        },
+        totalDeleted: {
+            type: [Date],
+            default: []
+        }
+    },
+    recipesDashboard: {
+        totalCreated: {
+            type: [Date],
+            default: []
+        },
+        totalDeleted: {
+            type: [Date],
+            default: []
+        }
+    },
+    reviewsDashboard: {
+        totalPosted: {
+            type: [Date],
+            default: []
+        },
+        totalDeleted: {
+            type: [Date],
+            default: []
+        }
+    }
+});
+
+const Dashboard = model("Dashboard", dashboardSchema);
+
+async function getSingleInstance() {
+    if (!(await Dashboard.findOne({}))) {
+        await Dashboard.create({});
+    }
+    return await Dashboard.findOne({});
+};
+
+module.exports = {
+    getSingleInstance
+};
