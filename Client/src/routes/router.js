ebeddd56dd88f2f1aa5bba37e5f71fa16a7bee4d
Akif Syuleyman <f17.8oy@gmail.com>
2023-12-11 02:59:03 +0200

    Combined routes in single file and exported router

---
+++ Client/src/routes/router.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from "vue-router";
+import NotFound from "../components/common/NotFound.vue";
+import { useEditModeStore } from "../stores/editMode";
+import { guestRoutes } from "./guestRoutes";
+import { commonRoutes } from "./commonRoutes";
+import { userRoutes } from "./userRoutes";
+import { adminRoutes } from "./adminRoutes";
+
+const routes = [
+    ...guestRoutes,
+    ...commonRoutes,
+    ...userRoutes,
+    ...adminRoutes,
+    {
+        path: '/:catchAll(.*)*',
+        component: NotFound
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+const componentsToCheckEditMode = ['RecipeDetails', 'Profile', 'EditRecipe'];
+
+router.beforeEach((to, from) => {
+    const fromComponentName = from.name || '';
+    const toComponentName = to.name || '';
+
+    if (componentsToCheckEditMode.includes(fromComponentName) && useEditModeStore().editMode) {
+        useEditModeStore().disableEditMode();
+    }
+    if (componentsToCheckEditMode.includes(toComponentName) && toComponentName !== 'RecipeDetails') {
+        useEditModeStore().enableEditMode();
+    }
+
+    return true;
+});
+
+export default router;
