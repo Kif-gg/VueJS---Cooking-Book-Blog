c629c94dc5bef1fb6d0708308cfc706fa9406da5
Akif Syuleyman <f17.8oy@gmail.com>
2023-12-11 03:17:45 +0200

    Added about component

---
+++ Client/src/components/common/About.vue
@@ -0,0 +1,69 @@
+<template>
+    <main class="about">
+        <img src="https://img.freepik.com/free-photo/preparing-salad-female-chef-cutting-fresh-vegetables-cooking-process-selective-focus_155003-15131.jpg"
+            alt="Chef John Doe">
+        <p>Hello, I'm Chef John Doe, the culinary artist behind this blog. With a passion for creating delightful
+            culinary experiences, I invite you to explore the world of flavors with me.</p>
+
+        <p>My culinary journey began in the charming town of Foodsville, where I discovered my love for the art of
+            cooking. I received formal training at the Culinary Institute of Excellence and have continued to refine
+            my
+            skills through hands-on experiences in various kitchens around the world.</p>
+
+        <p>At the heart of my cooking philosophy is the belief that great food goes beyond sustenance—it's an
+            expression
+            of creativity and love. I draw inspiration from global culinary traditions, incorporating these elements
+            into each dish I create.</p>
+
+        <p>One of my favorite aspects of cooking is experimenting with unique flavor combinations and creating
+            visual
+            delights that engage all the senses. I believe that cooking is not just a necessity but a form of art
+            that
+            brings joy to those who savor each bite.</p>
+
+        <p>Over the years, I've developed a particular affinity for Mediterranean cuisine, specializing in dishes
+            that
+            highlight the vibrant flavors of fresh herbs, olive oil, and sun-kissed vegetables. Some of my signature
+            creations include:</p>
+
+        <ul>
+            <li class="dish-card">
+                <img src="https://cheerfulchoices.com/wp-content/uploads/2023/04/Mediterranean-Quinoa-Stuffed-Peppers-9.jpeg"
+                    alt="Dish photo">
+                Mediterranean Stuffed Peppers
+            </li>
+
+            <li class="dish-card">
+                <img src="https://static01.nyt.com/images/2015/06/17/multimedia/clark-grilled-chicken/clark-grilled-chicken-superJumbo.jpg"
+                    alt="Dish photo">
+                Lemon Herb Grilled Chicken
+            </li>
+
+            <li class="dish-card">
+                <img src="https://culinarybutterfly.com/wp-content/uploads/2020/01/Caprese-Salad-facebook.png"
+                    alt="Dish photo">
+                Caprese Salad with Balsamic Glaze
+            </li>
+        </ul>
+
+        <p>These dishes encapsulate my culinary style, featuring a blend of bold flavors, creative presentation, and
+            a
+            touch of warmth that reflects my love for sharing good food.</p>
+
+        <p>Outside the kitchen, you'll find me exploring local farmers' markets, experimenting with new ingredients,
+            and
+            capturing the beauty of food through photography. I believe that a well-rounded life contributes to a
+            richer
+            culinary experience.</p>
+
+        <p>Thank you for joining me on this culinary journey. I'm excited to share my passion for cooking with you.
+            Feel
+            free to explore the recipes, and don't hesitate to connect with me on social media. Happy cooking!</p>
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
