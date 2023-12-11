d742acba9ab8a0326ece65c9d24276b178435d02
Akif Syuleyman <f17.8oy@gmail.com>
2023-12-11 03:20:45 +0200

    Added theme switcher (dark and light themes)

---
+++ Client/src/components/common/ThemeSwitch.vue
@@ -0,0 +1,28 @@
+<template>
+    <label for="theme-switcher" class="slider">
+        <input type="checkbox" name="theme" id="theme-switcher" @click="changeTheme($event)">
+        <div class="thumb"></div>
+    </label>
+</template>
+
+<script>
+export default {
+    methods: {
+        changeTheme(event) {
+            const body = document.getElementById("body");
+            if (event.target.checked) {
+                body.classList.add("dark-mode");
+                localStorage.setItem("Theme", "dark");
+            } else if (!event.target.checked) {
+                body.classList.remove("dark-mode");
+                localStorage.setItem("Theme", "light");
+            }
+        }
+    },
+    mounted() {
+        if (document.getElementById("body").classList.contains('dark-mode') && localStorage.getItem("Theme") == "dark") {
+            document.getElementById('theme-switcher').checked = true;
+        }
+    }
+}
+</script>
