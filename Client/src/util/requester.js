2f42e041be85cbb2e8065ca835adab7f454dba4a
Akif Syuleyman <f17.8oy@gmail.com>
2023-12-11 02:59:32 +0200

    Implemented requester with fetch api

---
+++ Client/src/util/requester.js
@@ -0,0 +1,33 @@
+const request = async (method, url, data) => {
+
+    const options = { method: method, mode: "cors", credentials: "include" };
+
+    if (method !== "GET" && data) {
+        options.headers = {
+            "content-type": "application/json",
+        };
+        options.body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, options);
+
+    if (response.status === 204) {
+        return;
+    }
+    
+    const result = await response.json();
+    
+    if (!response.ok) {
+        if (response.status === 401) {
+            localStorage.removeItem("Authenticated");
+            localStorage.removeItem("User");
+        }
+        throw new Error(result.message);
+    }
+    return result;
+};
+
+export const get = request.bind(null, "GET");
+export const post = request.bind(null, "POST");
+export const put = request.bind(null, "PUT");
+export const del = request.bind(null, "DELETE");
