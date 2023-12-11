0266ba2d948504a17bc61dfb23538ff74c22c7ca
Akif Syuleyman <f17.8oy@gmail.com>
2023-12-11 03:23:05 +0200

    Added logout for admin

---
+++ Client/src/components/admin-only/AdminLogout.vue
@@ -0,0 +1,32 @@
+<template>
+    <Loader v-if="isLoading"></Loader>
+</template>
+
+<script>
+import { useAuthenticatedStore } from '../../stores/authenticated';
+import { adminLogout } from '../../services/adminService'
+import Loader from '../common/Loader.vue';
+export default {
+    data() {
+        return {
+            isLoading: false
+        }
+    },
+    async created() {
+        try {
+            this.isLoading = true;
+            useAuthenticatedStore().setGuest();
+            await adminLogout();
+            await this.$router.replace("/login");
+        }
+        catch (error) {
+            await this.$router.replace("/");
+            alert(error.message);
+        }
+        finally {
+            this.isLoading = false;
+        }
+    },
+    components: { Loader }
+}
+</script>
