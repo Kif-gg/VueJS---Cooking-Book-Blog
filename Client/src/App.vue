95e4510f8d5996b34a393df322419336bd617f66
Akif Syuleyman <f17.8oy@gmail.com>
2023-12-11 02:46:54 +0200

    Used pinia and router on app, replaced default entry point

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,30 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <Header></Header>
+    <RouterView></RouterView>
+    <Footer></Footer>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import Header from './components/common/Header.vue';
+import Footer from './components/common/Footer.vue';
+import { RouterView } from 'vue-router';
+
+export default {
+    components: { Header, Footer, RouterView, },                                               
+    mounted() {
+        const theme = localStorage.getItem("Theme");
+        if (theme == "dark") {
+            document.getElementById("body").classList.add("dark-mode");
+            document.getElementById("theme-switcher").checked = true;
+        };
+    }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
--- Client/src/main.js
+++ Client/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
+import router from './routes/router';
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .use(createPinia())
+    .mount('#app');
